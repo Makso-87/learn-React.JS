c95e809f9d93297231dcd63b3e8d9990a39ee1a1
Makso-87 <makso16108@gmail.com>
2018-01-17 15:31:41 +0300

    Подключение и настройка препроцессора Less

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 var webpack = require("webpack");
-module.exports = {
+modules.exports = {
     entry : "./src/index.js",
     output : {
         path : __dirname + '/public',
         filename : 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                    loader: 'style!css!less'
+            }
+        ]
     }
 };
